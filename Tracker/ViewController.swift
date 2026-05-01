89d22ee6d38b98dab9d4c806b5bf8eee7a24408f
Vadim Kislov <kislov.v@gmail.com>
2026-05-01 13:31:02 +0300

    Initial project Tracker

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Kislov Vadim on 01.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
